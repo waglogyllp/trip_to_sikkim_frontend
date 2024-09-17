f403ee214dcfc3be99caf64edbd5de279bcf0613
Abhisek Adhikari <abhisekadhikari1906@gmail.com>
2024-09-17 15:54:09 +0530

    Instant Commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,18 @@
-const path = require('path');
+const path = require("path")
+const webpack = require("webpack")
 
 module.exports = function override(config, env) {
-  // Add custom Webpack configurations here
-  config.resolve.fallback = {
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "util": require.resolve("util/"),
-    "zlib": require.resolve("browserify-zlib"),
-    "stream": require.resolve("stream-browserify"),
-    "url": require.resolve("url/"),
-    "assert": require.resolve("assert/")
-  };
+    // Add custom Webpack configurations here
+    config.resolve.fallback = {
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        util: require.resolve("util/"),
+        zlib: require.resolve("browserify-zlib"),
+        stream: require.resolve("stream-browserify"),
+        url: require.resolve("url/"),
+        assert: require.resolve("assert/"),
+        crypto: require.resolve("crypto-browserify"),
+    }
 
-  return config;
-};
+    return config
+}
--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -9,24 +9,13 @@ import retu from "../images/return.svg"
 import bag from "../images/bag.svg"
 import { useParams } from "react-router-dom"
 import axios from "axios"
+// import Razorpay from "razorpay"
 
 const Booking = () => {
     const { slug } = useParams()
 
-    console.log(slug)
-
     const [data, setData] = useState([]) // Changed default value to null
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        phone: "",
-        travelDate: "",
-        travelers: "",
-        vehicle: "normal", // Setting a default value for the select field
-        specialRequest: "",
-        package_id: data._id,
-    })
+    const [packagePrice, setPackagePrice] = useState()
 
     const handleChange = (e) => {
         setFormData({
@@ -43,6 +32,7 @@ const Booking = () => {
 
             if (response.data.success) {
                 setData(response.data.data) // Correctly update the data state with package details
+                setPackagePrice(response.data.data.travelClass.normalCar.price) // Set the default package price
             } else {
                 setData(null) // If no success, set data to null
             }
@@ -56,41 +46,135 @@ const Booking = () => {
         getPackageDetails()
     }, [])
 
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        travelDate: "",
+        travelers: "",
+        vehicle: "normal", // Setting a default value for the select field
+        specialRequest: "",
+    })
+
+    const [order, setOrder] = useState({})
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            console.log(formData)
-
             const response = await axios.post(
                 `${process.env.REACT_APP_BASE_URL}/order/create`,
-                formData
+                {
+                    ...formData,
+                    package_id: data._id,
+                    total: packagePrice,
+                }
             )
 
-            console.log(response.response)
+            setOrder(response.data)
 
-            if (response.data.success) {
-                alert("Booking successful!")
+            if (!response.data.success) {
+                alert(response.data.message)
             }
+
+            /*  const rzpayOptions = {
+                key: "rzp_test_CbA7Z0GwqqJ6Dn",
+                amount: order.total * 100,
+                currency: "INR",
+                name: "Sikkim Tour",
+                description: "Payment for your Sikkim Tour",
+                image: "https://example.com/your_logo",
+                order_id: order.id,
+                handler: async function (response) {
+                    try {
+                        const paymentResponse = await axios.post(
+                            `${process.env.REACT_APP_BASE_URL}/order/verify`,
+                            {
+                                order_id: order.id,
+                                payment_id: response.razorpay_payment_id,
+                                signature: response.razorpay_signature,
+                            }
+                        )
+
+                        if (paymentResponse.data.success) {
+                            alert("Payment successful!")
+                        } else {
+                            alert("Payment failed. Please try again.")
+                        }
+                    } catch (e) {
+                        console.error("Error during payment:", e.message)
+                        alert("Payment failed. Please try again.")
+                    }
+                },
+                prefill: {
+                    name: formData.firstName + " " + formData.lastName,
+                    email: formData.email,
+                    contact: formData.phone,
+                },
+                notes: {
+                    address: "Sikkim Tour",
+                },
+                theme: {
+                    color: "#F37254",
+                },
+            }
+
+            const rpay = new Razorpay(rzpayOptions)
+
+            rpay.open() */
+
+            var options = {
+                key: "rzp_test_CbA7Z0GwqqJ6Dn", // Enter the Key ID generated from the Dashboard
+                amount: data.message.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+                currency: data.message.currency,
+                name: "AbhiTech", //your business name
+                description: "Test Transaction",
+                image: "https://0.gravatar.com/avatar/7fb6173c4d9f1de4dc75b11b4a6375dd0754ee9a76541f2414e3efc5506ba9a2?size=512",
+                order_id: data.message.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+                callback_url: "http://localhost:8000/order/verify",
+                redirect: true,
+                handler: async function (response) {
+                    console.log(response.razorpay_payment_id)
+                    console.log(response.razorpay_order_id)
+                    console.log(response.razorpay_signature)
+                    alert(response)
+                },
+                prefill: {
+                    //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+                    name: "Abhisek Adhikari", //your customer's name
+                    email: "abhisekadhikari1906@gmail.com",
+                    contact: "6296767187", //Provide the customer's phone number for better conversion rates
+                },
+                notes: {
+                    address: "My Address",
+                },
+                theme: {
+                    color: "#3399cc",
+                },
+            }
+            // const rzp1 = new Razorpay(options)
+            // rzp1.open()
         } catch (e) {
-            console.log("Error during booking:", e.response.data)
+            console.log("Error during booking:", e)
             // alert("An error occurred. Please try again.")
         }
     }
 
-  console.log(data)
-
-  return (
-    <div className="">
-      <section className="flex flex-col md:flex-row h-96 text-black p-4 md:p-0">
-        <div className="mt-12 md:mt-24 md:ml-20 text-left w-full md:w-1/2 flex flex-col justify-center">
-          <h1 className="text-3xl md:text-5xl font-bold">Book Now !!</h1>
-          <span className="text-3xl md:text-5xl font-bold mt-2 mb-6 block">
-            10% OFF
-          </span>
-          <p className="mb-6 text-base md:text-lg">
-            Let's Plan Your Perfect Trip To Sikkim With Us. Experience the magic
-            of Sikkim with our tailored travel packages.
-          </p>
+    return (
+        <div className="">
+            <section className="flex flex-col md:flex-row h-96 text-black p-4 md:p-0">
+                <div className="mt-12 md:mt-24 md:ml-20 text-left w-full md:w-1/2 flex flex-col justify-center">
+                    <h1 className="text-3xl md:text-5xl font-bold">
+                        Book Now !!
+                    </h1>
+                    <span className="text-3xl md:text-5xl font-bold mt-2 mb-6 block">
+                        10% OFF
+                    </span>
+                    <p className="mb-6 text-base md:text-lg">
+                        Let's Plan Your Perfect Trip To Sikkim With Us.
+                        Experience the magic of Sikkim with our tailored travel
+                        packages.
+                    </p>
 
                     <div className="flex flex-col md:flex-row justify-start space-y-4 md:space-y-0 md:space-x-4">
                         {/* Add any additional content or buttons here */}
@@ -186,28 +270,29 @@ const Booking = () => {
                                 className="p-3 w-full rounded-md border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-[#E69737]"
                             />
                         </div>
-<div className="mb-4">
-    <label className="block text-sm font-semibold mb-2">
-        Your Chosen Package
-    </label>
-    <div className="flex items-center p-3 w-full rounded-md border border-gray-300 shadow-sm">
-        {/* Package image */}
-        <img
-            src="https://your-image-url.com/package-image.jpg" // Replace with your image URL
-            alt="Package"
-            className="w-12 h-12 rounded-md mr-4"
-        />
-        {/* Package title */}
-        <input
-            type="text"
-            name="packageTitle"
-            value={formData.packageTitle}
-            onChange={handleChange}
-            className="w-full border-none focus:outline-none"
-            placeholder="Package title"
-        />
-    </div>
-</div>
+                        <div className="mb-4">
+                            <label className="block text-sm font-semibold mb-2">
+                                Your Chosen Package
+                            </label>
+                            <div className="flex items-center p-3 w-full rounded-md border border-gray-300 shadow-sm">
+                                {/* Package image */}
+                                <img
+                                    src={img} // Replace with your image URL
+                                    alt="Package"
+                                    className="w-12 h-12 rounded-md mr-4"
+                                />
+                                {/* Package title */}
+                                {/* <input
+                                    type="text"
+                                    name="packageTitle"
+                                    value={formData.packageTitle}
+                                    onChange={handleChange}
+                                    className="w-full border-none focus:outline-none"
+                                    placeholder="Package title"
+                                /> */}
+                                <p>{data.title}</p>
+                            </div>
+                        </div>
                         {/* Select Field */}
                         <div className="mb-4">
                             <label className="block text-sm font-semibold mb-2">
@@ -216,7 +301,24 @@ const Booking = () => {
                             <select
                                 className="p-3 w-full rounded-md border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-[#E69737]"
                                 value={formData.vehicle}
-                                onChange={handleChange}
+                                onChange={(e) => {
+                                    handleChange(e) // Update the form data
+                                    // Update the package price based on the selected vehicle type
+                                    const selectedVehicle = e.target.value
+                                    if (selectedVehicle === "normal") {
+                                        setPackagePrice(
+                                            data.travelClass.normalCar.price
+                                        )
+                                    } else if (selectedVehicle === "small") {
+                                        setPackagePrice(
+                                            data.travelClass.smallCar.price
+                                        )
+                                    } else if (selectedVehicle === "luxury") {
+                                        setPackagePrice(
+                                            data.travelClass.luxuryCar.price
+                                        )
+                                    }
+                                }}
                                 name="vehicle"
                             >
                                 <option value="normal">Normal Car</option>
@@ -237,6 +339,12 @@ const Booking = () => {
                                 onChange={handleChange}
                             ></textarea>
                         </div>
+
+                        <p>
+                            Total Price:{" "}
+                            <span className="font-bold">₹{packagePrice}</span>
+                        </p>
+
                         <button
                             type="submit"
                             className="w-full bg-[#3b5d50] text-white py-3 rounded-md hover:bg-[#D6892F] focus:outline-none focus:ring-2 focus:ring-[#E69737]"
